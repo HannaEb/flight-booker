d3dfb15285962aa1f4615e122cfe849774133937
Hanna <hannaeberhoefer@yahoo.com>
2020-06-15 16:47:41 +0100

    Create associations for airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.create(name: 'San Francisco', code: 'SFO')
+Airport.create(name: 'New York Area', code: 'NYC')
+Airport.create(name: 'Thessaloniki', code: 'SKG')
+Airport.create(name: 'Berlin', code: 'TXL')
+Airport.create(name: 'London Heathrow', code: 'LHR')
+Airport.create(name: 'Rio de Janeiro', code: 'GIG')
+Airport.create(name: 'Palermo', code: 'PMO')
+Airport.create(name: 'Buenos Aires', code: 'BAI')
+Airport.create(name: 'Stockholm', code: 'ARN')
+Airport.create(name: 'Toronto', code: 'YYZ')
+Airport.create(name: 'Tokyo', code: 'HND')
